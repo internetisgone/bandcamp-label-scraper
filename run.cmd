b0c7f5f65968f6252525b6061363f076b2caad37
internetisgone <schumann777@proton.me>
2023-12-16 13:41:26 +0800

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -4,8 +4,10 @@ echo pls be patient...
 if not exist ".venv" (
     py -m venv .venv
     call .venv\Scripts\activate.bat
-    pip install -r requirements.txt 
+    pip install -r requirements.txt
 ) else (
     call .venv\Scripts\activate.bat
-)   
+)
 py main.py
+echo you may now close this window
+pause
